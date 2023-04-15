b38fe5fd0cbd2e11a485b257b6d9e90906589f7e
用户名 <>
2023-04-15 19:11:50 +0800

    Site updated: 2023-04-15 19:11:49

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","3b46bc6e6f85ffc29d1108e823a3ad37"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
